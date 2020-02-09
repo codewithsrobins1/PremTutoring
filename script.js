d2f3f2bcf64548a8e6c2906d8b1264f79d0acfba
codewithsrobins1 <codewithsrobins1@gmail.com>
2020-02-09 12:22:26 -0800

    020920

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ new fullpage('#fullpage', {
     autoscrolling: true,
     navigation: true,
     anchors: ['section1', 'section2', 'section3', 'section4'],
-	navigationTooltips: ['Home', 'About', 'Service', 'Contact'],
+	navigationTooltips: ['Home','About', 'Service','Testimonials','Contact'],
 });
