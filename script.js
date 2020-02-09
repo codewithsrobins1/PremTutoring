7e227b917d01acbcccd6f5506007e4a0a8fad61b
Sean <47503402+codewithsrobins1@users.noreply.github.com>
2020-02-09 11:13:23 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+    //Navigation
+    autoscrolling: true,
+    navigation: true,
+    anchors: ['section1', 'section2', 'section3', 'section4'],
+	navigationTooltips: ['Home', 'About', 'Service', 'Contact'],
+});
